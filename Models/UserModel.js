57b232d64627e706a13eafadcbda172f803e6244
za-6174 <66186092+za-6174@users.noreply.github.com>
2024-03-20 22:13:23 +0300

    initialize backend and create user model

---
+++ Models/UserModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    }
+})
+
+userSchema.pre("save", async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
+})
+
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({email}) 
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth) {
+            return user;
+        }
+        else {
+            throw Error("Incorrect password");
+        }
+    }
+    else {
+        throw Error("Incorrect email");
+    }
+}
+
+module.exports = mongoose.model("Users", userSchema); 
